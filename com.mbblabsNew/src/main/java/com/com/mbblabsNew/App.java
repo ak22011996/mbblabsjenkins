d19adfb089145659986546044ce97377c21f43f3
Abhimanyu <Abhimanyu@host.docker.internal>
2025-08-11 09:01:15 +0000

    this is my first commit for jenkins

---
+++ com.mbblabsNew/src/main/java/com/com/mbblabsNew/App.java
@@ -0,0 +1,13 @@
+package com.com.mbblabsNew;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
